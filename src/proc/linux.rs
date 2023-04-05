26ee20505df46d8a08cc0b0d437305bd0e563820
Paul Mabileau <paulmabileau@hotmail.fr>
2023-04-05 13:50:56 +0200

    Feat: Implement process collection for Linux
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/proc/linux.rs
+++ src/proc/linux.rs
@@ -1,7 +1,25 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 
 use crate::proc::ProcInfo;
 
 pub fn collect_procs() -> Result<Vec<ProcInfo>> {
-    Ok(vec![])
+    let mut res = Vec::new();
+
+    for p in procfs::process::all_processes()? {
+        let p = p?;
+        let uid = p.uid()?;
+        res.push(ProcInfo {
+            pid: p.pid,
+            uid,
+            name: p.cmdline()?.first().unwrap_or(&String::new()).clone(),
+            user: users::get_user_by_uid(uid)
+                .ok_or_else(|| anyhow!("User {uid} not found."))?
+                .name()
+                .to_str()
+                .ok_or_else(|| anyhow!("Invalid name."))?
+                .to_string(),
+        });
+    }
+
+    Ok(res)
 }
