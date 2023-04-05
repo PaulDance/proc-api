4edf69aec8792d3ee405d2d271f1004b0ccb67d8
Paul Mabileau <paulmabileau@hotmail.fr>
2023-04-05 18:57:01 +0200

    Feat: Implement process collection route
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+use std::sync::{Arc, Mutex};
+
 use anyhow::Result;
+use warp::Filter;
 
 mod proc;
 use proc::ProcInfo;
 
-fn main() -> Result<()> {
-    dbg!(ProcInfo::collect_all()?);
-    Ok(())
+#[tokio::main]
+async fn main() {
+    let procs = Arc::new(Mutex::new(Vec::<ProcInfo>::new()));
+
+    warp::serve(
+        warp::path("acquire_process_list")
+            .and(warp::filters::method::post())
+            .map(move || {
+                *procs.lock().unwrap() = ProcInfo::collect_all().unwrap();
+                ""
+            }),
+    )
+    .run(([127, 0, 0, 1], 8080))
+    .await;
 }
